ecf2a0ab64512b0cfa4815ad27f142d33e1d55ef
tidvn <tiendung0325@gmail.com>
2024-10-18 16:43:20 +0700

    updade title

--- components/app/IdeascalePage.tsx
+++ components/app/IdeascalePage.tsx
@@ -7,7 +7,7 @@ export default function IdeascalePage() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-3xl md:leading-14">
-            Our Ideascale Projects
+            Our Ideascale project in fund13
           </h1>
         </div>
         <div className="container py-12">
