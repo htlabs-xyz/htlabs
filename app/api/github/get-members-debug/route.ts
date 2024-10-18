35db5677cda4e5cb04a88f60d2387dca7575f664
tidvn <tiendung0325@gmail.com>
2024-10-18 14:53:20 +0700

    add debug

---
+++ app/api/github/get-members-debug/route.ts
@@ -0,0 +1,89 @@
+import { Member } from 'app/type'
+import axios from 'axios'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
+  const githubAccessToken: string = process.env.GH_TOKEN || ''
+  const query: string = `
+    query 
+{
+  organization(login: "htlabs-xyz") {
+    membersWithRole(first: 100) {
+      edges {
+        node {
+          login
+          name
+          avatarUrl
+          email
+          company
+          location
+          bio
+          websiteUrl
+          socialAccounts(first: 5) {
+            edges {
+              node {
+                provider
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+  `
+
+  try {
+    const { data } = await axios.post(
+      'https://api.github.com/graphql',
+      { query },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${githubAccessToken}`,
+        },
+      }
+    )
+
+    if (data.errors) {
+      throw new Error(data.errors[0].message)
+    }
+
+    const response = data.data.organization.membersWithRole.edges.map((edge) => {
+      const node = edge.node
+      return {
+        name: node.name,
+        github: node.login,
+        avatar: node.avatarUrl,
+        company: node.company,
+        website: node.websiteUrl,
+        location: node.location,
+        email: node.email,
+        bio: node.bio,
+        social: node.socialAccounts.edges.map((edge) => {
+          if (edge.node.provider === 'GENERIC' && edge.node.url.includes('https://t.me/')) {
+            return {
+              provider: 'telegram',
+              url: edge.node.url,
+            }
+          }
+          return {
+            provider: edge.node.provider.toLowerCase(),
+            url: edge.node.url,
+          }
+        }),
+      } as Member
+    })
+    return Response.json({
+      members: response,
+      message: 'success',
+    })
+  } catch (error) {
+    return Response.json({
+      members: [],
+      message: error.message,
+    })
+  }
+}
