67b4294abf498932e5b22052581c036891867c2d
tidvn <tiendung0325@gmail.com>
2024-10-18 16:21:35 +0700

    update

--- components/common/Image.tsx
+++ components/common/Image.tsx
@@ -2,8 +2,9 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  const isExternal = typeof src === 'string' && src.startsWith('http')
+  return <NextImage src={isExternal ? src : `${basePath || ''}${src}`} {...rest} />
+}
 
 export default Image
