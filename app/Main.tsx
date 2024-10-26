860910f9104f67a0a13c30746bc9fdb8c5e6ca0f
tidvn <tiendung0325@gmail.com>
2024-10-27 01:03:25 +0700

    fix build

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,5 +1,5 @@
 import Link from '@/components/ui/link'
-import Tag from '@/components/Tag'
+import Tag from '@/components/common/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
